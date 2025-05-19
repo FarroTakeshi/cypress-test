df77987a7b09db9bab8b645cb141162d8f2f5893
Takeshi Santiago Farro Hinoshita <tfarrohi@emeal.nttdata.com>
2025-05-19 12:49:18 -0500

    agregar prueba para login fallido

--- cypress/e2e/login/swag_login.feature
+++ cypress/e2e/login/swag_login.feature
@@ -4,3 +4,10 @@ Feature: Inicio de sesión en la Página de SwagLabs
     When introduzco las credenciales correctas
     And hago clic en el botón de inicio de sesión
     Then debería ser redirigido a la página de Products
+
+  Scenario: Login Fallido
+    Given visito la web de SwagLabs
+    When introduzco las credenciales incorrectas
+    And hago clic en el botón de inicio de sesión
+    Then debería mostrarse un mensaje de error
+    And debería mostrarse con un fondo de color "#e2231a"
--- cypress/e2e/login/swag_login.js
+++ cypress/e2e/login/swag_login.js
@@ -1,4 +1,5 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { hexToRgb } from "../../support/utils/functions"
 
 Given("visito la web de SwagLabs", () => {
   cy.visit('https://www.saucedemo.com/');
@@ -9,6 +10,11 @@ When("introduzco las credenciales correctas", () => {
   cy.get("input#password").type("secret_sauce");
 });
 
+When("introduzco las credenciales incorrectas", () => {
+  cy.get("input#user-name").type("standard_user");
+  cy.get("input#password").type("bad_password");
+});
+
 When("hago clic en el botón de inicio de sesión", () => {
   cy.get("#login-button").click();
 });
@@ -17,4 +23,13 @@ Then("debería ser redirigido a la página de Products", () => {
   cy.url().should('eq', 'https://www.saucedemo.com/inventory.html');
   cy.location('pathname').should('eq', '/inventory.html');
   cy.get('[data-test="title"]').should('contain.text', 'Products');
+});
+
+Then("debería mostrarse un mensaje de error", () => {
+  cy.get("h3[data-test=error]").contains("Username and password do not match any user in this service");
+});
+
+Then("debería mostrarse con un fondo de color {string}", (hexadecimal_code) => {
+  cy.get(".error-message-container.error")
+    .should('have.css', 'background-color', hexToRgb(hexadecimal_code))
 });
